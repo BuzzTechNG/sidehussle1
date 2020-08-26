e634389137c2bb879efc29ffb01242f15bf71ec9
Richard Omoyele <Richardadesina26@gmail.com>
2020-08-26 10:11:22 -0700

    Two input boxes arranged per role

--- src/Components/Dashboard-components/Mainpage/Updateuserinfo.jsx
+++ src/Components/Dashboard-components/Mainpage/Updateuserinfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const updateInfo = (props) => {
+const Updateinfo = (props) => {
   return (
     <div>
       <div className="pt-5" style={{ zIndex: "0" }}>
@@ -16,19 +16,37 @@ const updateInfo = (props) => {
         <div className="my-4">
           <div className="mcard p-4">
             {/*  */}
-            <div className="job-form">
+            <div className="job-form row">
               <div>
                 {" "}
                 <i className="fa fa-edit"></i> Enter your fullname
               </div>
-              <input type="text" placeholder="FirstName LastName(Surname)" />
+              <div className="col-sm-12 col-md-4 px-2 m-2">
+                <input type="text" placeholder="FirstName" />
+              </div>
+              <div className="col-sm-12 col-md-4 px-2 m-2">
+                <input type="text" placeholder=" Middlename" />
+              </div>
+              <div className="col-sm-12 col-md-4 px-2 m-2">
+                <input type="text" placeholder="LastName(Surname)" />
+              </div>
             </div>
             {/*  */}
-            <div className="job-form">
-              <div>
-                <i className="fa fa-edit"></i> Enter Your phone number
+            <div className="job-form row">
+              <div className="col-sm-12 col-md-6 px-2 m-2">
+                <div>
+                  <i className="fa fa-edit"></i> Update your phone number
+                </div>
+
+                <input type="number" placeholder="Enter Phone number" />
               </div>
-              <input type="number" placeholder="Phone number" />
+              <div className="col-sm-12 col-md-6 px-2 m-2">
+                <div>
+                  <i className="fa fa-edit"></i> Update your Email address
+                </div>
+
+                <input type="mail" placeholder="Enter E-mail address" />
+              </div>
             </div>
             {/*  */}
             <div className="job-form">
@@ -60,4 +78,4 @@ const updateInfo = (props) => {
     </div>
   );
 };
-export default updateInfo;
+export default Updateinfo;
